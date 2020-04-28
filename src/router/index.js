164d3620b3f6f14edcd5073489cb049ddb175936
unknown <1529588564@qq.com>
2020-04-28 21:32:50 +0800

    test

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import test from '../components/test/test.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+	   path:'/',
+	   component:test
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
