58ae2762ed6d3095fe601f97b2d089a28abf4877
unknown <1529588564@qq.com>
2020-04-29 16:04:37 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import test from '../components/test/test.vue'
 import home from '../views/home/index.vue'
-<<<<<<< HEAD
 import homeRouter from './home/index.js'
-=======
-import PictureRoter from './tupian'
->>>>>>> be6625057c045ae288ba4d29bb765a5e8cccdaaf
+
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -17,21 +15,11 @@ const routes = [
 }]
 
 const router = new VueRouter({
-<<<<<<< HEAD
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes
-=======
+
   mode: 'history',
   base: process.env.BASE_URL,
-  routes:[
-	  PictureRoter,
-	 {
-		 path:'/*',
-		 redirect:'/tupian'
-	 }
-  ]
->>>>>>> be6625057c045ae288ba4d29bb765a5e8cccdaaf
+  routes
+
 })
 
 export default router
