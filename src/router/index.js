4ff8b6c3fedd94a936c31cda3e4d8b8a2f773865
unknown <2364039798@qq.com>
2020-04-30 17:07:58 +0800

    xxx

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,26 @@ import VueRouter from 'vue-router'
 import test from '../components/test/test.vue'
 import headerRouter from './header/index.js'
 import loginRouter from './login/index.js'
-
-
 Vue.use(VueRouter)
 
-const routes = [
-	headerRouter,
-	loginRouter,
-	// systemRouter,
-	{
-	path: '/',
-	redirect: '/login'
-}]
+  const routes = [	
+	 headerRouter,
+	 loginRouter,
+	 // systemRouter,
+	 {
+	 path: '/',
+	 redirect: '/login'
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+
+
 
 
 export default router
