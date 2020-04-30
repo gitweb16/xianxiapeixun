e669b18208f960ae2d467c172f445be1713b7386
xutao <2499266030@qq.com>
2020-04-30 16:18:58 +0800

    xxx

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import test from '../components/test/test.vue'
-<<<<<<< HEAD
-import home from '../views/home/index.vue'
-import PictureRoter from './xutao/tupian'
-import MenuRoter from './xutao/caidan'
-import PremiumRoter from './xutao/tuifei'
-=======
 import headerRouter from './header/index.js'
 
-
-
->>>>>>> 03c268080b5acd73e402e2f2667682da67a840ee
 Vue.use(VueRouter)
 
 const routes = [
@@ -21,25 +12,5 @@ const routes = [
 	path: '/',
 	redirect: '/header'
 }]
-
-const router = new VueRouter({
-
-  mode: 'history',
-  base: process.env.BASE_URL,
-<<<<<<< HEAD
-  routes:[
-	 PictureRoter,
-	 MenuRoter,
-	 PremiumRoter,
-	 {
-		 path:'/*',
-		 redirect:'/xutao/caidan'
-	 }
-  ]
-=======
-  routes
-
->>>>>>> 03c268080b5acd73e402e2f2667682da67a840ee
-})
 
 export default router
